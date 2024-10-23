8d4c0e4dc420fc49da7455166b410c4a3f307f2a
nauar123 <nauarbahaa@gmail.com>
2024-10-23 18:01:47 +0200

    Initial commit

---
+++ src/ordre.java
@@ -0,0 +1,18 @@
+public class ordre {
+
+
+    private int ordreNr;
+    private int dato;
+    private int tid;
+    // Artiputterne til opgaven.
+
+    public ordre() // default constructor
+    {}
+
+    public int getOrdreNr()
+    {
+        return ordreNr;
+    }
+
+
+}
