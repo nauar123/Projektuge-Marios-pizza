707ce64cdda2f0c060fc1bce49d7d524f81194e6
nauar123 <nauarbahaa@gmail.com>
2024-10-24 12:38:16 +0200

    Oprettelse af pizzamenu
    Har lavet en arraylist med pizza navnene

--- src/Menukort.java
+++ src/Menukort.java
@@ -1,5 +1,49 @@
+import java.util.ArrayList;
+
 public class Menukort {
 
+public static void main (String[]args){
+
+    ArrayList<String> pizzaMenu = new ArrayList<String>();
+
+    pizzaMenu.add(1,"Magaritha");
+    pizzaMenu.add(2,"Pepperoni");
+    pizzaMenu.add(3,"Hawaiian");
+    pizzaMenu.add(4,"BBQ Chicken");
+    pizzaMenu.add(5,"Vegetarian");
+    pizzaMenu.add(6,"Meat Lovers");
+    pizzaMenu.add(7,"Four Cheese");
+    pizzaMenu.add(8,"Buffalo Chicken");
+    pizzaMenu.add(9,"Supreme");
+    pizzaMenu.add(10,"Mexican");
+    pizzaMenu.add(11,"Seafood Special");
+    pizzaMenu.add(12,"Tuna and Sweetcorn");
+    pizzaMenu.add(13,"Spicy Sausage");
+    pizzaMenu.add(14,"Prosciutto and Rocket");
+    pizzaMenu.add(15,"Mushroom and Truffle Oil");
+    pizzaMenu.add(16,"Calzone");
+    pizzaMenu.add(17,"Carbonara Pizza");
+    pizzaMenu.add(18,"Pesto and Chicken");
+    pizzaMenu.add(19,"Gorgonzola and Pear");
+    pizzaMenu.add(20,"Salami and Olive");
+    pizzaMenu.add(21,"Spinach and Ricotta");
+    pizzaMenu.add(22,"Greek Pizza (Feta and Olives)");
+    pizzaMenu.add(23,"Zucchini and Goat Cheese");
+    pizzaMenu.add(24,"Egg and Bacon");
+    pizzaMenu.add(25,"Shrimp and Pesto");
+    pizzaMenu.add(26,"Tandoori Chicken");
+    pizzaMenu.add(27,"Pastrami Pizza");
+    pizzaMenu.add(28,"Bolognese Pizza");
+    pizzaMenu.add(29,"Quattro Stagioni");
+    pizzaMenu.add(30,"Lamb and Mint");
+
+
+    ArrayList<Double> pizzaPrices = new ArrayList<>();
+    pizzaPrices.add()
+
+
+
+}
 
 
 }
